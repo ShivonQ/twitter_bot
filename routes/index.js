e9b33839c85ac648762ee997511e6a75cdb91334
ShivonQ <realitykeeper@gmail.com>
2016-03-23 15:51:04 -0500

    Fixed the Package.json data dependancies.  Still not getting the displayment of my page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-// router.get('/', function(req,res,next){
-//   res.render('')
-// })
+ router.get('/allDungeons', function(req,res,next){
+   res.render('allDungeons', {title: 'Dungeons'})
+ });
 module.exports = router;

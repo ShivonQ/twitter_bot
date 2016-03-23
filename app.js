1f294d783de4a4508c04f950f127ca92dff510be
Clara <minneapolis-edu@users.noreply.github.com>
2016-03-23 13:50:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var MongoClient=require("mongodb".MongoClient);
+var MongoClient=require("mongodb").MongoClient;
 var engines = require("jade");
 var assert = require("assert");
 var path = require('path');

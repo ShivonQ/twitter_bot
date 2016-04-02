a0f76b7c0fe7cd6526ca5f9d8add438d8304289d
ShivonQ <realitykeeper@gmail.com>
2016-04-02 13:28:51 -0500

    Dungeon Gen is working, Creating tweet manufacturing components.  Once done will migrate them to their own file.
    app.js--> Set Interval(24 hrs){ create a single dungeon object
                    -->Determine how many tweets(Number) and How Often (Milliseconds in a 23 hour period)
                    -->Dungeon Tweet(Number of rooms, date created[maybe], number of tweets to finish, how often to tweet] ("In one hour I will begin tweeting the other rooms")
                    -->room Tweets[start with "Room #"],

--- app.js
+++ app.js
@@ -129,19 +129,33 @@ setInterval(function(){
 var a_day=86400000;
 var ten_minutes=600000;
 
-//var temp_dung=new Dungeon();
-//var immediate_dungeon=new Dungeon();
-//console.log(immediate_dungeon);
-//db.collection.save(immediate_dungeon);
-//setInterval(function(){
-//var new_dungeon=dung_generator;
-//  db.collection.save(new_dungeon)
-//}, ten_minutes);
-
 var dun = Dungeon_Gen;
 console.log(dun);
+
+var how_many_tweets=function(JSON_object_string_length){
+  var tweet_limit=140;
+  //by adding one here I take care of any stragglers from the rounding.
+  return Math.floor(JSON_object_string_length/tweet_limit)+1;
+};
+var how_often_to_tweet=function(number_of_tweets_required){
+  var twenty_three_hours=82800000;
+  return Math.floor(twenty_three_hours/number_of_tweets_required);
+}
 var string_dun=JSON.stringify(dun);
+var length_of_object=string_dun.length;
 console.log(string_dun);
+console.log("The Length of that string was: "+length_of_object);
+var final_number_of_tweets = how_many_tweets(length_of_object);
+console.log(how_often_to_tweet(final_number_of_tweets)+" milliseconds is how often This twitter bot will tweet to tell the whole dungeon")
+console.log("There will be "+final_number_of_tweets+" tweets once this is working.")
+var seperate_rooms_from_dungeon=function(dun){
+  for (var k=0;k<dun.all_rooms.length;k++){
+    console.log("Room Number: "+ dun.all_rooms[k].room_number+"\n"+JSON.stringify(dun.all_rooms[k]));
+    console.log("This many tweets for a room: "+how_many_tweets(JSON.stringify(dun.all_rooms[k]).length+14));
+    //console.log("Room Number: "+JSON.stringify(room.room_number)+"\n"+ JSON.stringify(room))
+  }
+}
+seperate_rooms_from_dungeon(dun);
 module.exports = app;
 
 // Major Furnishings

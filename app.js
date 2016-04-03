e31d479539be59364b38b9e7e0ba8bf36c2492c3
ShivonQ <realitykeeper@gmail.com>
2016-04-03 10:29:31 -0500

    Dungeon Gen is working, Creating tweet manufacturing components.  Once done will migrate them to their own file. Deungeon to DB is working.
    app.js--> Set Interval(24 hrs){ create a single dungeon object
                    -->Determine how many tweets(Number) and How Often (Milliseconds in a 23 hour period)
                    -->Dungeon Tweet(Number of rooms, date created[maybe], number of tweets to finish, how often to tweet] ("In one hour I will begin tweeting the other rooms")
                    -->room Tweets[start with "Room #"],

--- app.js
+++ app.js
@@ -115,44 +115,30 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-//setInterval(function(){
-//  var timePost=String(new Date);
-//  var testPost="| Dimensions: 20 x 30 | Walls: Worked Stone | Water Source: Fountain | Doors: N & S & E Walls | Trap: Yes [ECL+2] | Encounter: Yes [ECL-1] |"
-//  client.post('statuses/update',{status: timePost},function(error,tweet,response){
-//    if(!error){
-//      console.log(tweet);
-//    }
-//    else{
-//      console.log(error);
-//    }
-//  });
-//}, 600000);
-//var a_day=86400000;
-//var ten_minutes=600000;
-//var insert_dun_to_db=function(a_dungeon){
-//  var new_dungeon_to_db= Dungeon({
-//    date_created: a_dungeon.date_created,
-//    dungeon_id:a_dungeon.dungeon_id,
-//    number_of_rooms:a_dungeon.number_of_rooms,
-//    all_rooms:a_dungeon.all_rooms
-//  });
-//  new_dungeon_to_db.save(function(err){
-//    if(err) {console.log("An error occurred while trying to save the new dungeon to the database.")}
-//  })
-//};
-//var dun = Dungeon_Gen;
-//insert_dun_to_db(dun);
-//console.log(dun);
-//
-//var how_many_tweets=function(JSON_object_string_length){
-//  var tweet_limit=140;
-//  //by adding one here I take care of any stragglers from the rounding.
-//  return Math.floor(JSON_object_string_length/tweet_limit)+1;
-//};
-//var how_often_to_tweet=function(number_of_tweets_required){
-//  var twenty_three_hours=82800000;
-//  return Math.floor(twenty_three_hours/number_of_tweets_required);
-//}
+
+var a_day=86400000;
+var ten_minutes=600000;
+var insert_dun_to_db=function(a_dungeon){
+  var new_dungeon_to_db= Dungeon({
+    date_created: a_dungeon.date_created,
+    dungeon_id:a_dungeon.dungeon_id,
+    number_of_rooms:a_dungeon.number_of_rooms,
+    all_rooms:a_dungeon.all_rooms
+  });
+  new_dungeon_to_db.save(function(err){
+    if(err) {console.log("An error occurred while trying to save the new dungeon to the database.")}
+  })
+};
+
+var how_many_tweets=function(JSON_object_string_length){
+  var tweet_limit=140;
+  //by adding one here I take care of any stragglers from the rounding.
+  return Math.floor(JSON_object_string_length/tweet_limit)+1;
+};
+var how_often_to_tweet=function(number_of_tweets_required){
+  var twenty_three_hours=82800000;
+  return Math.floor(twenty_three_hours/number_of_tweets_required);
+}
 //var string_dun=JSON.stringify(dun);
 //var length_of_object=string_dun.length;
 //console.log(string_dun);
@@ -160,18 +146,44 @@ app.use(function(err, req, res, next) {
 //var final_number_of_tweets = how_many_tweets(length_of_object);
 //console.log(how_often_to_tweet(final_number_of_tweets)+" milliseconds is how often This twitter bot will tweet to tell the whole dungeon")
 //console.log("There will be "+final_number_of_tweets+" tweets once this is working.");
-//var seperate_rooms_from_dungeon=function(dun){
-//  var total_tweets_for_all_rooms=0;
-//  for (var k=0;k<dun.all_rooms.length;k++){
-//    console.log("Room Number: "+ dun.all_rooms[k].room_number+"\n"+JSON.stringify(dun.all_rooms[k]));
-//    console.log(JSON.stringify(dun.all_rooms[k]).length+14);
-//    console.log("This many tweets for a room: "+how_many_tweets(JSON.stringify(dun.all_rooms[k]).length+14));
-//    total_tweets_for_all_rooms+=how_many_tweets(JSON.stringify(dun.all_rooms[k]).length+14);
-//    //console.log("Room Number: "+JSON.stringify(room.room_number)+"\n"+ JSON.stringify(room))
-//  }
-//return total_tweets_for_all_rooms;
-//};
-//seperate_rooms_from_dungeon(dun);
+var seperate_rooms_from_dungeon=function(dun){
+  var total_tweets_for_all_rooms=0;
+  for (var k=0;k<dun.all_rooms.length;k++){
+    console.log("Room Number: "+ dun.all_rooms[k].room_number+"\n"+JSON.stringify(dun.all_rooms[k]));
+    console.log(JSON.stringify(dun.all_rooms[k]).length+14);
+    console.log("This many tweets for a room: "+how_many_tweets(JSON.stringify(dun.all_rooms[k]).length+14));
+    total_tweets_for_all_rooms+=how_many_tweets(JSON.stringify(dun.all_rooms[k]).length+14);
+    //console.log("Room Number: "+JSON.stringify(room.room_number)+"\n"+ JSON.stringify(room))
+  }
+return total_tweets_for_all_rooms;
+};
+setInterval(function(){
+  //This is the 24 hour interval function
+  var dun = null;
+  if(dun==null){
+    dun=Dungeon_Gen;
+  }
+
+  var final_number_of_tweets = how_many_tweets(JSON.stringify(dun).length);
+  console.log("This will take "+final_number_of_tweets+" Tweets to fully output.")
+  var how_often_to_tweet_variable=how_often_to_tweet(final_number_of_tweets);
+  insert_dun_to_db(dun);
+  //FIGURE OUT THE INTERVAl
+  //TODO uncomment and then run this aspect
+  setInterval(function(){
+    //  THIS IS THE variable INTERVAL FUNCTION:  FIGURE OUT HOW MANY TWEETS determined by sting length
+
+  },how_often_to_tweet_variable);
+  client.post('statuses/update',{status: "A"},function(error,tweet,response){
+    if(!error){
+      console.log(tweet);
+    }
+    else{
+      console.log(error);
+    }
+  },82800000);
+  dun=null;
+}, 30000000);
 module.exports = app;
 
 // Major Furnishings
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,54 @@ router.get('/allDungeons', function(req, res, next) {
         return res.render('allDungeons',{Dungeons:dungeonsDocs, error:req.flash('error')});
     });
 });
+router.get('/3_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:3},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/4_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:4},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/5_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:5},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/6_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:6},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/7_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:7},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/8_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:8},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/9_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:9},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
+router.get('/10_rooms',function(req,res,next){
+    Dungeon.find({number_of_rooms:10},function(err, dungeonDocs){
+        if(err){return next(err); }
+        return res.render('allDungeons', {Dungeons:dungeonDocs, error:req.flash('error in 3 rooms')})
+    });
+});
 router.post('/', function(req,res,next){
     var new_dungeon=Dungeon
 })
